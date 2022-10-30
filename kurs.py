11ec6d1cec3303c6628c4f9e29663a28425a1cc9
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-10-30 14:09:11 +0100

    introducing to python print, variables...

---
+++ kurs.py
@@ -0,0 +1,45 @@
+
+# # print("cao svimaaaaaaaaaaaaaaaa")
+# # print znaci stampaj, ispis u terminalu 
+# print("Ovo je kurs pajtona.")
+
+# print("Zdravo svete!")
+# print("Zdravo svete!")
+
+# # kada ispisujemo recnicu potrebni su nam navodnici
+# # neki broj tad mozemo a i ne moramo da ih imamo
+# print("2022")
+# print(2022)
+# print("5554cvgywyvugvyuwg")
+# print("5+5")
+
+
+# broj = 10
+# broj2 = 5
+# broj3 = 8
+
+# moja_skola = "jovan"
+# #  moja skola = "jovan"    NE MOZE SPACE
+
+# mojaSkola = "jovan"
+
+# broj = 5
+# Broj = 6
+# bRoj = 7
+# brOj = 8
+# broJ = 9
+
+# print(moja_skola)
+
+
+brojJedan = 12
+brojDva = 13
+
+
+print(brojJedan + brojDva)
+print(brojJedan - brojDva)
+print(brojJedan * brojDva)
+print(brojJedan / brojDva)
+
+
+
