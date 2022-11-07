89dab17b704a0b0da9c754e3bf6f34e6610db091
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-11-07 20:12:06 +0100

    class 5/11

--- kurs.py
+++ kurs.py
@@ -1,3 +1,13 @@
+# ------------ uvod
+# introduction
+# vsc
+# python
+
+
+# ---------- 1. cas
+
+# print
+# varijble, pravila imenovanja
 
 # # print("cao svimaaaaaaaaaaaaaaaa")
 # # print znaci stampaj, ispis u terminalu 
@@ -32,14 +42,112 @@
 # print(moja_skola)
 
 
-brojJedan = 12
-brojDva = 13
+# brojJedan = 12
+# brojDva = 13
 
 
-print(brojJedan + brojDva)
-print(brojJedan - brojDva)
-print(brojJedan * brojDva)
-print(brojJedan / brojDva)
+# print(brojJedan + brojDva)
+# print(brojJedan - brojDva)
+# print(brojJedan * brojDva)
+# print(brojJedan / brojDva)
+
+
+
+# ------------- 2. cas
+
+
+# obnavljanje 
+# unos sa tastature
+# tipovi podataka
+# razlicte vrste ispisa
+# zadaci
+
+
+# 1. ars
+# 2. povrsina kv ili prav
+# 3. pretvaranje iz eur u din
+# 4. ime, prezime, skola ispis
+
+
+
+# print znaci ispis ili stampaj i to se odnsoi na terminal
+# print("ovo je kurs pajtona")
+# print("caooom svimA")
+
+
+# moja_skola = "gimnazija"
+# broj = 5854556
+
+# print(broj)
+
+# broj = 5
+# drugi_broj = 6
+
+# print(broj + drugi_broj)
+# print(broj - drugi_broj)
+#     #    5    *     6
+# print(broj * drugi_broj)
+
+# ------------------------------------------
+
+# string, str
+# varijabla = "dxastxfsctxtas8959859@#@#!@!##@!"
+# var1 = "sacgugcyugcvsdvv"
+# var2 = "421"
+# # integer, int, celi broj
+# brojJedan = 5
+# broj3 = -65899
+
+# # float
+# brojDva = 6.54
+
+# ime = "Said"  
+# godina = 18
+# print("Moje ime je " + ime)
+# print("Ja imam", godina, "bhabcsuhyasbchgascuyusa")
+
+
+# ime = "Petar"
+# prezime = "Vucinic"
+
+# print("Moje ime je", ime, "a moje prezime je", prezime)
+# print("Moje ime je " + ime + " a moje prezime je " + prezime)
+
+
+# ja imam --- godina i idem u ------- skolu.
+
+# skola = "gimnazija"
+# godina = 17
+
+# print("Ja imam", godina, "godina i idem u", skola, "skolu")
+# print("Ja imam " , godina , " godina i idiem u " + skola + " skolu")
+
+# --------------------
+
+# broj = int(input("unesite neki broj: "))
+# print("uneti broj je", broj)
+
+# --------------------
+
+# ime = str(input("Unesite vase ime: "))
+# print("Moje ime je", ime)
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
